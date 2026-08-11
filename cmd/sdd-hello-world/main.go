27dc7c2560f283db20c231d6258fba73433a1851
Petar Djukic <petar.djukic@gmail.com>
2026-08-10 20:13:22 -0400

    Add hello world binary entry point (GH-85)
    Implements srd001-hello-world-binary R1.1, R2.1, R2.2, R2.3.
    
    Closes #86
    
    Stats:
      Lines of code (Go, production): 6 (+6)
      Lines of code (Go, tests):      0 (+0)
      Words (documentation):          0 (+0)
    
    Skill: do-work
    Called-by: gh-issue-pop

---
+++ cmd/sdd-hello-world/main.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2026 Petar Djukic. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Command sdd-hello-world prints a fixed greeting and exits.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
