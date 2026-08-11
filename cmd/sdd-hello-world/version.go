5e8a289f3d0f1bf2259f8d9e4e4cd4f7b86cf602
Petar Djukic <petar.djukic@gmail.com>
2026-08-10 20:20:07 -0400

    Revert "Release 02.0: Hello World binary from specifications (GH-85)"
    This reverts commit 02faa99c31724fbd320563c799e7dee0110caea5, reversing
    changes made to d5f190c845ca86c4649f524f5b2b83e4525e1936.

--- cmd/sdd-hello-world/version.go
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2026 Petar Djukic. All rights reserved.
-// SPDX-License-Identifier: MIT
-
-package main
-
-// Version is set during the generation process.
-const Version = "0.0.1"
